7d383a7fc057bf8c9d6d21bc0d461b0bf4582821
Rana Rajput <protectmango@gmail.com>
2025-08-30 03:28:35 +0530

    code of spi and i2c driver with rtc

--- I2C/i2c_driver.c
+++ I2C/i2c_driver.c
@@ -63,3 +63,49 @@ The address mapping for the DS1307 is as follows:
 0x07: Control Register 
  
  */
+// header.h
+#define CSO (1<<7)
+extern void spi0_init(void);
+typedef unsigned short int ushort;
+typedef unsigned char us;
+us spi0(us data);
+ushort read_mcp3204(us ch_num);
+
+// mcp3204-driver.c
+#include <lpc21xx.h>
+#include "header.h"
+
+ushort read_mcp3204(us ch_num) {
+    us byteL = 0, byteH = 0;
+    ushort result = 0;
+    ch_num <<= 6; // set the ch-num
+
+    IOCLR0 = CSO; // select slave
+    spi0(0x06); // start bit, SGL mode, D2
+    byteH = spi0(ch_num); // CH select
+    byteL = spi0(0x00);
+    IOSET0 = CSO; // deselect slave
+
+    byteH &= 0x0F; // mask higher nibble
+    result = (byteH << 8) | byteL; // merge result
+    return result;
+}
+
+// main_mcp3204.C
+#include "header.h"
+
+main() {
+    ushort temp;
+    uarto_init(9600);
+    spi0_init();
+    uarto_tx_string("MCP3204 Testing \r\n");
+
+    while(1) {
+        temp = read_mcp3204(0); // read CH0
+        uarto_tx_integer(temp);
+        uarto_tx_string("\r\n");
+        delay_ms(50); // optional
+    }
+}
+
+
--- SPI/spi_driver.c
+++ SPI/spi_driver.c
@@ -37,6 +37,22 @@ ABRT : Slave Abort Flag.(bit 3)
 
  */
 
+#include < lpc21xx.h>
+#include "header.h"
+
+void spio-init(void) {
+    PINSEL0 = 0x1500; // P0.4 -> SCKO, P0.5 -> MISOO, P0.6 -> MOSI
+    IODIR0 |= CSO; // P0.7 is output direction
+    IOSET0 = CSO; // deselect slave
+    S0SPCR = 0x20; // CPOL=CPHA=0, select master mode, MSB first
+    S0SPCCR = 0x15; // SPI Clock is 1 Mbps
+}
+
+us spio(us data) {
+    SOSPDR = data;
+    while(SPIF == 0); // waiting for SPI transfer
+    return SOSPDR;
+}
 
 
 /*
@@ -47,4 +63,47 @@ MCP3209 External ADC
 
 
 */
+// header.h
+#define CSO (1<<7)
+extern void spio-init(void);
+typedef unsigned short int ushort;
+typedef unsigned char us;
+us spio(us data);
+ushort read_mcp3204(us ch_num);
 
+// mcp3204-driver.c
+#include <lpc21xx.h>
+#include "header.h"
+
+ushort read_mcp3204(us ch_num) {
+    us byteL = 0, byteH = 0;
+    ushort result = 0;
+    ch_num <<= 6; // set the ch-num
+
+    IOCLR0 = CSO; // select slave
+    spio(0x06); // start bit, SGL mode, D2
+    byteH = spio(ch_num); // CH select
+    byteL = spio(0x00);
+    IOSET0 = CSO; // deselect slave
+
+    byteH &= 0x0F; // mask higher nibble
+    result = (byteH << 8) | byteL; // merge result
+    return result;
+}
+
+// main_mcp3204.C
+#include "header.h"
+
+main() {
+    ushort temp;
+    uarto-init(9600);
+    spio-init();
+    uarto-tx-string("MCP3204 Testing \r\n");
+
+    while(1) {
+        temp = read_mcp3204(0); // read CH0
+        uarto-tx-integer(temp);
+        uarto-tx-string("\r\n");
+        delay_ms(50); // optional
+    }
+}
