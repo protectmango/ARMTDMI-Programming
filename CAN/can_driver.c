f3665ef284bc35609049f927967166f3eede3857
Rana Rajput <protectmango@gmail.com>
2025-08-30 03:21:20 +0530

    can protocol theory

---
+++ CAN/can_driver.c
@@ -0,0 +1,26 @@
+/*CAN Protocol(ISO 11898)*/
+
+/*
+ Two wire communication CANH , CANL
+
+ Message Based Protocol (frames)
+
+ Data Frame
+ Remote Frame
+ Error Frame
+ Overload Frame
+
+ Every data frame has a unique identifier.
+
+ CAN speed and distance 
+ 
+ 1 Mbps over 40 meter network.
+
+ CAN Version
+
+ Standard CAN (2.0A): Uses an 11-bit identifier.
+ Extended CAN (2.0B): Uses a 29-bit identifier.
+
+
+
+ */
