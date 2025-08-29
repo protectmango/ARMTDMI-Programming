fd3b0063d71e80b06cf695109cff5cbd21c107cf
Rana Rajput <protectmango@gmail.com>
2025-08-30 03:01:12 +0530

    i2c protocol instructions

---
+++ I2C/i2c_driver.c
@@ -0,0 +1,65 @@
+/*I2C Driver*/
+
+/*
+I2C : Inter Integrated Circuit
+
+Two Wire
+
+SDA : Serial Data Line.
+SCL : Serial Clock Line.
+
+Data Frame in I2C
+
+Byte write frame : write 1 byte of data from master to slave.
+Byte read frame : read 1 byte of data from slave to master.
+Sequential read frame : used to read multiple byte of data from slave to master.
+
+I2C Bus Frame and State
+
+START  
+STOP 
+ACK 
+NACK 
+
+START → Slave Address + Read/Write bit → ACK → Memory Address → ACK → Data → ACK → STOP
+
+
+I2C Registers
+
+I2CONSET : I2C Control Set Register.
+
+I2CONCLR : I2C Control Clear Register.
+
+I2STAT : I2C Status Register.
+
+I2DAT : I2C Data Register.
+
+I2SCLH/I2SCLL : I2C Serial Clock High/Low.
+
+
+ */
+
+
+/*DS1307 RTC*/
+
+/*
+ 64 byte onchip memory
+ 	- 8 Mapped time keeping registers.
+	- 56 General Purpose registers.
+	
+
+To ensure the correct time and date, an external crystal oscillator with a value of 
+32.768 kHz must be connected to the RTC.
+
+The address mapping for the DS1307 is as follows:
+
+0x00: Seconds (0-59) 
+0x01: Minutes (0-59) 
+0x02: Hours (1-12 or 0-23) 
+0x03: Day (1-7) 
+0x04: Date (1-31) 
+0x05: Month (1-12) 
+0x06: Year (0-99) 
+0x07: Control Register 
+ 
+ */
